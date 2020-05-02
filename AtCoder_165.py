fc46d8bf2f2d4d96233ad370ce6b8cd53d2d39d2
n@yamada <yamadanao@ny-MacBook-Pro.local>
2020-05-03 01:12:22 +0900

    Initital Commit

--- AtCoder_165.py
+++ AtCoder_165.py
@@ -23,3 +23,38 @@ while a < X:
   i += 1
 
 print(i)
+
+# C
+N, M, Q = map(int, input().split())
+l = []
+for i in range(Q):
+  l.append(list(map(int, input().split())))
+
+A_N = [[i] for i in range(1, M+1)]
+for i in range(N-1):
+  a = []
+  for j in range(len(A_N)):
+    last = A_N[j][-1]
+    for k in range(last, M+1):
+      a.append(A_N[j] + [k])
+  A_N = a
+
+high_score = 0
+for i in range(len(A_N)):
+  A = A_N[i]
+  score = 0
+  for j in range(len(l)):
+    aj, bj, cj, dj = l[j]
+    if A[bj-1]-A[aj-1] == cj:
+      score += dj
+  high_score = max(high_score, score)
+
+print(high_score)
+
+# D
+import math
+A, B, N = map(int, input().split())
+
+x = min(B-1, N)
+y = math.floor(A*x/B) - A * math.floor(x/B)
+print(y)
